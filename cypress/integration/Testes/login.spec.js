7b51ea96b12e930cf2303265ebbf6e6cbf12283b
FenrirSif <41023381+FenrirSif@users.noreply.github.com>
2022-07-21 14:34:52 -0300

    Teste usuario logado

--- cypress/integration/Testes/login.spec.js
+++ cypress/integration/Testes/login.spec.js
@@ -89,8 +89,10 @@ describe('Teste funcional', () => {
         cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__warging').should('have.text','É campo obrigatório')
     })
     it.only('Login validação com erro',()=>{
-        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type('teste@gmail.com')
-        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('123')
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default')
+            .type('teste@gmail.com')
+        cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default')
+            .type('123')
         cy.get('.otUnI').click()
         cy.get('#modalText').should('contain','Usuário ou senha inválido')
         cy.get('#btnCloseModal').click()
